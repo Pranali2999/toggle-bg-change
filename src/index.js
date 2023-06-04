718420195b76885db5faf9993b245ee3656cff42
Pranali2999 <78157763+Pranali2999@users.noreply.github.com>
2023-06-04 23:34:14 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+var slider = document.getElementById("slide");
+var txt = document.getElementById("txt");
+slider.addEventListener("click", function () {
+  if (document.body.style.background === "white") {
+    document.body.style.background = "black";
+    txt.style.color = "white";
+  } else {
+    document.body.style.background = "white";
+    txt.style.color = "black";
+  }
+});
